a01fefb178c009223fa1ae95b459f301700a0ac6
Sayed Ali Sena <131773898+Alisenaqubadi@users.noreply.github.com>
2025-12-03 22:23:24 +0500

    project was completed

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,5 +4,6 @@ import rootController from '../controllers/index.js'
 const rootRouter = Router()
 
 rootRouter.get('/', rootController.root)
+rootRouter.post('/', rootController.save)
 
 export default rootRouter;
